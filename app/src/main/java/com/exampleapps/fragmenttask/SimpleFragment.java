16621b1f8706ee8c95c788e8408f763f61e793c5
nauvalpradipta <nauvalpradipta2@gmail.com>
2023-02-28 00:35:56 +0700

    add static fragment

---
+++ app/src/main/java/com/exampleapps/fragmenttask/SimpleFragment.java
@@ -0,0 +1,29 @@
+package com.exampleapps.fragmenttask;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link SimpleFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class SimpleFragment extends Fragment {
+
+
+    public SimpleFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        final View rootView = inflater.inflate(R.layout.fragment_simple, container, false);
+        // Inflate the layout for this fragment
+        return rootView;
+    }
+}
